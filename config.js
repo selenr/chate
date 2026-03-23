00562e822cfd772b9058541805b46c2d48fd372e
selenr <kecappanasdong@gmail.com>
2026-03-24 06:37:27 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    profileImage: "https://i.imgur.com/QKamZn4.jpeg",   // ganti dengan URL gambar profil
+    background: "url('https://i.imgur.com/7PGQf5k.jpeg') center/cover",
+    whatsappLink: "https://tapthelink.cfd/bTthF",
+    telegramLink: "https://tapthelink.cfd/bTthF"
+};
