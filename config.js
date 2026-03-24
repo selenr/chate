fbe31ce2f7eef60ab81cb70eb591ea79f6a4e977
selenr <kecappanasdong@gmail.com>
2026-03-24 07:26:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 const config = {
-    profileImage: "https://i.imgur.com/QKamZn4.jpeg",   // ganti dengan URL gambar profil
-    background: "url('https://i.imgur.com/7PGQf5k.jpeg') center/cover",
+    profileImage: "https://i.imgur.com/QKamZn4.jpeg",
+
+    backgrounds: [
+        "https://i.imgur.com/7PGQf5k.jpeg",
+        "https://images2.imgbox.com/a2/d0/PQWBiPeU_o.jpg"
+    ],
+
     whatsappLink: "https://tapthelink.cfd/bTthF",
     telegramLink: "https://tapthelink.cfd/bTthF"
 };
