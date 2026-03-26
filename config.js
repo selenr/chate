5a3b3cc6c6975a46aabf4b1d2e0a284361d0d4f4
selenr <kecappanasdong@gmail.com>
2026-03-27 02:16:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     profileImage: "https://images2.imgbox.com/b0/28/RlehZTyk_o.jpg",
 
     backgrounds: [
-        "https://https://images2.imgbox.com/fa/77/TwnEc3Cj_o.jpg",
+        "https://images2.imgbox.com/fa/77/TwnEc3Cj_o.jpg",
         "https://images2.imgbox.com/6f/43/sUmUfZl7_o.jpg"
     ],
 
